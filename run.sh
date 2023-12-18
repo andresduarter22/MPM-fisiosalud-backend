b59f6a14fb6db6861f06fe40d127ea219443ea26
andresduarter22 <andresduarter13@gmail.com>
2023-12-18 11:43:16 -0400

    adding wsgi to backend

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-python3 manage_database.py
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem -subj "/C=SI/ST=La Paz/L=La Paz/O=Fisiosalud/OU=IT Department/CN=www.example.com"
+
+#uwsgi --master --https 0.0.0.0:443,cert.pem,key.pem -p 4 -w wsgi:FisiosaludAPI.app
+
+uwsgi --master --https 0.0.0.0:5000,cert.pem,key.pem -p 4 --log-master -w wsgi:app
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from main.api.api_endpoints import FisiosaludAPI
+
+app = FisiosaludAPI().run()
